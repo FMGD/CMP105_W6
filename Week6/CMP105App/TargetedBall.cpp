84a1c58851f0b3ddf1a0d801a2df6a114a5be8b6
FMGD <55237715+FMGD@users.noreply.github.com>
2020-02-26 18:58:05 +0000

    Exercise 4
    Object accelerates towards the mouse cursor

--- Week6/CMP105App/TargetedBall.cpp
+++ Week6/CMP105App/TargetedBall.cpp
@@ -5,7 +5,15 @@ TargetedBall::TargetedBall()
 {
 	speed_ = 50.0f;
 	acceleration_ = 50.0f;
-	//target_ = sf::Vector2f(600, 300);
+	scale_ = 200.0f;
+	target_ = sf::Vector2f(600, 300);
+
+	velocity = sf::Vector2f(0, 0);
+
+	// For initial velocity != 0
+	//direction_ = target_ - getPosition();
+	//direction_ = Vector::normalise(direction_);
+	//velocity = (direction_ * speed_);
 }
 
 // Destructor
@@ -17,22 +25,9 @@ TargetedBall::~TargetedBall()
 // Functions
 void TargetedBall::update(float dt)
 {
-	target_ = sf::Vector2f(window_->getSize().x - this->getSize().x, window_->getSize().y - this->getSize().y); // Set target end right of window
-
-	//Calculate direction and move
-	direction_ = target_ - getPosition();
-	direction_ = Vector::normalise(direction_);
-
-	//velocity = (direction_ * speed_); // fixed speed
-	velocity += (direction_ * acceleration_) * dt; // acceleterated towards the point
-	setPosition(getPosition() + (velocity * dt));
-
-	// If object is close enough to target
-	if (Vector::magnitude(target_ - getPosition()) < 10.0f)
-	{
-		setPosition(target_);
-	}
-
+	//target_ = sf::Vector2f(window_->getSize().x - this->getSize().x, window_->getSize().y - this->getSize().y); // Set target to end right of window
+	target_ = sf::Vector2f(input->getMouseX() - (getSize().x / 2), input->getMouseY() - (getSize().y / 2));  // Set target to position of cursor
+	moveToTarget(dt);
 }
 
 
@@ -45,4 +40,33 @@ void TargetedBall::handleInput(float dt)
 void TargetedBall::setWindow(sf::RenderWindow* hwnd)
 {
 	window_ = hwnd;
+}
+
+
+void TargetedBall::moveToTarget(float dt)
+{
+	//Calculate direction
+	direction_ = target_ - getPosition();
+	direction_ = Vector::normalise(direction_);
+
+	// Object with not acceleration
+	//velocity = (direction_ * speed_); // v = u  (fixed speed)
+	//sf::Vector2f pos = velocity * dt; // s = v*t
+
+	// Object with acceleration
+	velocity += (direction_ * acceleration_ ) * dt; // v = u + a*t (accelerate towards the point)
+	sf::Vector2f pos = velocity * dt + 0.5f * (direction_ * acceleration_ ) * dt * dt; // s = ut + 1/2*a*t^2
+
+	std::cout << "Velocity x: " << velocity.x << " Velocity y: " << velocity.y  << "\n";
+	
+	//Move
+	setPosition(getPosition() + pos);
+
+	// If object is close enough to target
+	if (Vector::magnitude(target_ - getPosition()) < 10.0f)
+	{
+		setPosition(target_);
+		velocity = sf::Vector2f(0, 0);
+	}
+
 }
--- Week6/CMP105App/TargetedBall.h
+++ Week6/CMP105App/TargetedBall.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "Framework/GameObject.h"
 #include "Framework/Vector.h"
+#include <iostream>
 class TargetedBall :	public GameObject
 {
 private:
 	sf::RenderWindow* window_;
 	float speed_;
 	float acceleration_;
+	float scale_;
 	sf::Vector2f direction_;
 	//sf::Vector2f velocity_;
 	sf::Vector2f target_;
@@ -22,6 +24,7 @@ public:
 	void setWindow(sf::RenderWindow* hwnd);
 	void update(float dt) override;
 	void handleInput(float dt) override;
+	void moveToTarget(float dt);
 
 };
 
